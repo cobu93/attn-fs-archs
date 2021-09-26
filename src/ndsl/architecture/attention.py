Fixing CategoricalOHE wrong shape

src/ndsl/architecture/attention.py
@@ -48,7 +48,7 @@ class CategoricalOneHotEncoder(FeatureEncoder):
         self.embedding = nn.utils.weight_norm(nn.Linear(n_labels, output_size))
 
     def forward(self, src):
-        src = F.one_hot(src.long(), num_classes=self.n_labels).float()
+        src = F.one_hot(src.squeeze().long(), num_classes=self.n_labels).float()
         return self.embedding(src)
 
 class NumericalEncoder(FeatureEncoder):