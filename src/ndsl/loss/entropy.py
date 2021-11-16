4f4c8610cbf9fe2cc27fd8df75769a59c2a94391
cobu93 <urielcoro@outlook.com>
2021-11-16 06:40:53 -0600

    Adding entropy regularization

--- src/ndsl/loss/entropy.py
+++ src/ndsl/loss/entropy.py
@@ -14,12 +14,12 @@ class CELWithWeightsEntropyMinimization(nn.CrossEntropyLoss):
         # Entropy for each row
         weights_row_entropy = -torch.sum(weights * torch.log(weights + 1e-13), dim=-2)
         weights_row_entropy = torch.mean(weights_row_entropy)
-
+        
         # Entropy for each column
-        column_norm_weights = weights / weights.sum(dim=-1, keepdims=True)
+        column_norm_weights = weights / (weights.sum(dim=-1, keepdims=True) + 1e-13)
         weights_columns_entropy = -torch.sum(column_norm_weights * torch.log(column_norm_weights + 1e-13), dim=-1)
         weights_columns_entropy = torch.mean(weights_columns_entropy)
-
+        
         loss = loss + self.beta_1 * weights_row_entropy + self.beta_2 * weights_columns_entropy
         return loss
 
